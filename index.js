4259ef795ccc56eabb0d247d8c7945531aeb9668
Kiuliumov <miti.bania@gmail.com>
2023-12-02 18:19:34 +0200

    Event and Command handler

---
+++ index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+const { DISCORD_TOKEN: token } = process.env;
+
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+
+const eventsPath = path.join(__dirname, 'Events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'Commands');
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`Error with ${filePath}`);
+    }
+}
+
+client.login(token);
