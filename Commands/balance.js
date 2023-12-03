c0d189cf06dcc275c327e7b20397e10a387ebdcc
Kiuliumov <miti.bania@gmail.com>
2023-12-03 20:42:56 +0200

    Update balance.js

---
+++ Commands/balance.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("A basic ping command"),
+  async execute(interaction, profileData) {
+    const { balance } = profileData;
+    const username = interaction.user.username
+  },
+};
